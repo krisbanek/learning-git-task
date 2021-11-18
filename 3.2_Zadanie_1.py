3b72065561babbb8f4bd5eabfa0f0284658f1fb1
Krzysztof Banek <krisbanek@gmail.com>
2021-11-18 22:03:37 +0100

    Add print Commit_no1

--- 3.2_Zadanie_1.py
+++ 3.2_Zadanie_1.py
@@ -8,3 +8,5 @@ lista_zakupow_dict1 = {k: [v.capitalize() for v in lista_zakupow_dict[k] ] for k
 for shop in lista_zakupow_dict1:
     print(f"Ide do {shop.capitalize()}, kupuję tu następujące rzeczy {lista_zakupow_dict1[shop]}")
 print(f"W sumie kupuję {len(lista_zakupow_dict1[shop]) * len(lista_zakupow_dict1)} produktów")
+
+print("Commit_no1")
