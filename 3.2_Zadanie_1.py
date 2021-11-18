077bff047affca3f2f9899f9cd0805c76d5a693f
Krzysztof Banek <krisbanek@gmail.com>
2021-11-18 21:01:01 +0100

    Add final printing loop

--- 3.2_Zadanie_1.py
+++ 3.2_Zadanie_1.py
@@ -4,3 +4,7 @@ lista_zakupow_dict = {
 }
 
 lista_zakupow_dict1 = {k: [v.capitalize() for v in lista_zakupow_dict[k] ] for k in lista_zakupow_dict }
+
+for shop in lista_zakupow_dict1:
+    print(f"Ide do {shop.capitalize()}, kupuję tu następujące rzeczy {lista_zakupow_dict1[shop]}")
+print(f"W sumie kupuję {len(lista_zakupow_dict1[shop]) * len(lista_zakupow_dict1)} produktów")
